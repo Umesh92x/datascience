ddaaef641337caf98d1d6c758065d356c28df295
Umesh <37169232+Umesh92x@users.noreply.github.com>
2023-07-17 22:37:50 +0530

    Update InfinityListComprehension.py

--- Python/InfinityListComprehension.py
+++ Python/InfinityListComprehension.py
@@ -4,3 +4,26 @@ input_list = [1,2,3]
 [input_list.append(value) for value in input_list] # taking item from input list and appending into into the same list  
 
 Output -> Keep runnning....
+
+''' The solution to add timeout '''
+
+
+import signal
+
+def timeout_handler(signum, frame):
+    raise TimeoutError("Code execution timed out")
+
+try :
+    # Set the timeout to 5 seconds
+    signal.signal(signal.SIGALRM, timeout_handler) # timeout_handler function will be called when it gets a number say 5 seconds.
+    signal.alarm(5)
+
+    input_list = [1,2,3]
+    [input_list.append(value) for value in input_list] 
+    # Disable the timeout
+    signal.alarm(0)
+    
+except TimeoutError:
+    print("Code execution timed out due to over runnig for 5 mins")
+
+output -> Code execution timed out due to over runnig for 5 mins
