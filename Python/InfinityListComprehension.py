b4dd1d37c697e32e347676bb8e8643cafa35662b
Umesh <37169232+Umesh92x@users.noreply.github.com>
2023-07-17 22:21:01 +0530

    Create InfinityListComprehension.py

---
+++ Python/InfinityListComprehension.py
@@ -0,0 +1,6 @@
+# how to create a infinity Infinity List Comprehension
+
+input_list = [1,2,3]
+[input_list.append(value) for value in input_list] # taking item from input list and appending into into the same list  
+
+Output -> Keep runnning....
